d043fc0f9f3060bbf6887803ee0279ef7a7af3ad
Sean Marlow <sean.marlow@suse.com>
2017-11-07 12:48:39 -0700

    Add tests for job log filter.

--- test/unit/logging_filter_test.py
+++ test/unit/logging_filter_test.py
@@ -1,6 +1,10 @@
 from collections import namedtuple
+from mock import Mock
 
-from mash.logging_filter import SchedulerLoggingFilter
+from mash.logging_filter import (
+    SchedulerLoggingFilter,
+    JobFilter
+)
 
 
 class TestSchedulerLoggingFilter(object):
@@ -19,3 +23,18 @@ class TestSchedulerLoggingFilter(object):
             msg='maximum number of running instances reached'
         )
         assert self.log_filter.filter(record) is False
+
+
+class TestJobFilter(object):
+    def setup(self):
+        self.log_filter = JobFilter()
+        self.record = Mock()
+        self.record.msg = 'Log Message!'
+        self.record.job_id = '123'
+
+    def test_filter_with_job_id(self):
+        assert self.log_filter.filter(self.record) is True
+
+    def test_filter_no_job_id(self):
+        del self.record.job_id
+        assert self.log_filter.filter(self.record) is False
