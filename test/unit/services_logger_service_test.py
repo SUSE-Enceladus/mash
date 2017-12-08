80af66175e52c568dd9051859169cd81da49e3f5
Sean Marlow <sean.marlow@suse.com>
2017-12-08 16:41:47 -0700

    Update tests for logger service.

--- test/unit/services_logger_service_test.py
+++ test/unit/services_logger_service_test.py
@@ -2,6 +2,8 @@ import io
 import json
 import sys
 
+from amqpstorm import AMQPError
+
 from mock import MagicMock, Mock, patch
 from pytest import raises
 
@@ -25,42 +27,63 @@ class TestLoggerService(object):
         self.config = Mock()
         self.config.get_log_file.return_value = '/tmp/file.log'
 
-        self.message = {
-            "levelname": "INFO",
-            "msg": u"INFO 2017-11-01 11:36:36.782072 "
-                   "LoggerService \n Job[4711]: Test log message! \n"
-        }
-
         self.channel = Mock()
-        self.channel.basic.ack.return_value = None
-
-        self.logger = LoggerService()
-        self.logger.channel = self.channel
-        self.logger.close_connection = Mock()
-
         self.tag = Mock()
         self.method = {'delivery_tag': self.tag}
 
+        body = {
+            "levelname": "INFO",
+            "msg": u"INFO 2017-11-01 11:36:36.782072 "
+                   "LoggerService \n Job[4711]: Test log message! \n",
+            "job_id": "4711"
+        }
+        self.message = MagicMock(
+            body=json.dumps(body),
+            channel=self.channel,
+            method=self.method
+        )
+
+        self.logger = LoggerService()
+        self.logger.channel = self.channel
+
+    @patch.object(LoggerService, 'stop')
+    @patch.object(LoggerService, 'start')
     @patch.object(LoggerConfig, '__init__')
     @patch.object(LoggerService, '_bind_logger_queue')
     @patch.object(LoggerService, '_process_log')
     @patch.object(LoggerService, 'consume_queue')
     def test_logger_post_init(
         self, mock_consume_queue, mock_process_log,
-        mock_bind_logger_queue, mock_logger_config
+        mock_bind_logger_queue, mock_logger_config, mock_start, mock_stop
     ):
         mock_logger_config.return_value = None
+
+        # Test normal run
         self.logger.post_init()
+
         mock_consume_queue.assert_called_once_with(
             mock_process_log, mock_bind_logger_queue.return_value
         )
         mock_bind_logger_queue.assert_called_once_with(
             queue_name='mash.logger', route='mash.logger'
         )
-        self.logger.channel.start_consuming.side_effect = KeyboardInterrupt
+        mock_start.assert_called_once_with()
+        mock_stop.assert_called_once_with()
+
+        # Test keyboard interrupt
+        mock_start.side_effect = KeyboardInterrupt
+        mock_stop.reset_mock()
+
         self.logger.post_init()
-        self.logger.channel.stop_consuming.assert_called_once_with()
-        self.logger.close_connection.assert_called_once_with()
+        mock_stop.assert_called_once_with()
+
+        # Test unandled exception
+        mock_start.side_effect = Exception('Unable to connect.')
+
+        with raises(Exception) as e:
+            self.logger.post_init()
+
+        assert 'Unable to connect.' == str(e.value)
 
     @patch.object(LoggerService, '_declare_direct_exchange')
     @patch.object(LoggerService, '_declare_queue')
@@ -83,8 +106,9 @@ class TestLoggerService(object):
         )
 
     def test_logger_process_invalid_log(self):
+        self.message.body = ''
         with raises(MashLoggerException):
-            self.logger._process_log('', self.channel, self.method, Mock())
+            self.logger._process_log(self.message)
 
     @patch('os.path.exists')
     def test_logger_process_append(
@@ -92,13 +116,10 @@ class TestLoggerService(object):
     ):
         mock_path_exists.return_value = True
         self.logger.config = self.config
-        self.message['job_id'] = '4711'
 
         with patch(open_name, create=True) as mock_open:
             mock_open.return_value = MagicMock(spec=io.IOBase)
-            self.logger._process_log(
-                json.dumps(self.message), self.channel, self.method, Mock()
-            )
+            self.logger._process_log(self.message)
             file_handle = mock_open.return_value.__enter__.return_value
             file_handle.write.assert_called_with(
                 u'INFO 2017-11-01 11:36:36.782072 '
@@ -111,7 +132,6 @@ class TestLoggerService(object):
     ):
         mock_path_exists.return_value = True
         self.logger.config = self.config
-        self.message['job_id'] = '4711'
 
         with patch(open_name, create=True) as mock_open:
             mock_open.return_value = MagicMock(spec=io.IOBase)
@@ -119,6 +139,28 @@ class TestLoggerService(object):
             file_handle.write.side_effect = Exception('Error writing file!')
 
             with raises(MashLoggerException):
-                self.logger._process_log(
-                    json.dumps(self.message), self.channel, self.method, Mock()
-                )
+                self.logger._process_log(self.message)
+
+    def test_logger_start(self):
+        self.channel.consumer_tags = []
+        self.logger.channel = self.channel
+
+        self.logger.start()
+        self.channel.start_consuming.assert_called_once_with()
+
+    @patch.object(LoggerService, '_open_connection')
+    def test_logger_start_exception(self, mock_open_connection):
+        self.channel.start_consuming.side_effect = [AMQPError('Broken!'), None]
+        self.channel.consumer_tags = []
+        self.logger.channel = self.channel
+
+        self.logger.start()
+        mock_open_connection.assert_called_once_with()
+
+    @patch.object(LoggerService, 'close_connection')
+    def test_logger_stop(self, mock_close_connection):
+        self.logger.channel = self.channel
+
+        self.logger.stop()
+        self.channel.stop_consuming.assert_called_once_with()
+        mock_close_connection.assert_called_once_with()
