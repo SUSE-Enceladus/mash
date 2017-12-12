fdffe0793b80eb0f2ba854c0925fd0b643d1ba2c
Sean Marlow <sean.marlow@suse.com>
2017-12-12 12:11:39 -0700

    Add unit tests for testing service.

---
+++ test/unit/services/testing/testing_config_test.py
@@ -0,0 +1,13 @@
+from mash.services.testing.config import TestingConfig
+
+
+class TestTestingConfig(object):
+    def setup(self):
+        self.empty_config = TestingConfig('../data/empty_testing_config.yml')
+
+    def test_config_data(self):
+        assert self.empty_config.config_data
+
+    def test_get_log_file(self):
+        assert self.empty_config.get_log_file() == \
+            '/var/log/mash/testing_service.log'
