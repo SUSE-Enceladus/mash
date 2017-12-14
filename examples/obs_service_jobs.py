5922061ca6da4be8bceb1a9004c93f43f9e98f0e
Marcus Schäfer <ms@suse.de>
2017-12-14 15:45:21 +0100

    Fixed typo in obs_service_jobs example

--- examples/obs_service_jobs.py
+++ examples/obs_service_jobs.py
@@ -13,15 +13,15 @@ channel.queue.declare(queue='obs.service_event', durable=True)
 channel.basic.publish(
     exchange='obs', routing_key='service_event', mandatory=True, body='{"obsjob_delete": "0815"}'
 )
+
 channel.basic.publish(
     exchange='obs', routing_key='service_event', mandatory=True, body='{"obsjob":{"id": "0815","project": "Virtualization:Appliances:Images:Testing_x86","image": "test-image-iso","utctime": "now"}}'
 )
+
 channel.basic.publish(
     exchange='obs', routing_key='service_event', mandatory=True, body='{"obsjob":{"id": "4711","project": "Virtualization:Appliances:Images:Testing_x86","image": "test-image-docker","utctime": "always"}}'
 )
 
-listen_to_queue = 'obs.listener_0815'
-channel.queue.declare(queue=listen_to_queue, durable=True)
 channel.basic.publish(
     exchange='obs', routing_key='service_event', mandatory=True, body='{"obsjob_listen": "0815"}'
 )
