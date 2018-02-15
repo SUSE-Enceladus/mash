ed008858ddbdc807bef427c925106920fb25d5d1
Sean Marlow <sean.marlow@suse.com>
2018-02-15 13:57:28 -0700

    Update testing service ec2_job class.
    - Remove credential handling methods.
    - Update run_tests to run region tests in parallel with threads.
    - Update attrs for credentials changes.

--- test/unit/services/testing/ec2_job_test.py
+++ test/unit/services/testing/ec2_job_test.py
@@ -1,97 +1,19 @@
-import jwt
-
-from pytest import raises
 from unittest.mock import call, patch
 
-from mash.mash_exceptions import MashTestingException
 from mash.services.testing.ec2_job import EC2TestingJob
 
 
 class TestEC2TestingJob(object):
     def setup(self):
         self.job_config = {
-            'account': 'account',
-            'distro': 'SLES',
             'id': '1',
             'provider': 'ec2',
+            'test_regions': {'us-east-1': 'test-aws'},
             'tests': 'test_stuff',
-            'utctime': 'now'
+            'utctime': 'now',
         }
 
-    def test_get_credential_request(self):
-        job = EC2TestingJob(**self.job_config)
-        request = job._get_credential_request()
-
-        decoded = jwt.decode(
-            request,
-            'mash',
-            algorithm='HS256'
-        )
-
-        assert 'credentials' in decoded
-        assert decoded['credentials']['csp'] == 'ec2'
-        assert decoded['credentials']['account'] == 'account'
-
-    @patch('mash.services.testing.ec2_job.jwt')
-    def test_process_credentials(self, mock_jwt):
-        mock_jwt.decode.return_value = {
-            'credentials': {
-                'secret_access_key': '123',
-                'access_key_id': 'key123',
-                'ssh_key_name': 'temp_key',
-                'ssh_private_key': '123456789'
-            }
-        }
-
-        job = EC2TestingJob(**self.job_config)
-        job._process_credentials('fake_msg')
-
-        assert job.secret_access_key == '123'
-        assert job.access_key_id == 'key123'
-        assert job.ssh_key_name == 'temp_key'
-        assert job.ssh_private_key == '123456789'
-
-    @patch('mash.services.testing.ec2_job.jwt')
-    def test_process_credentials_error(self, mock_jwt):
-        mock_jwt.decode.return_value = {
-            'credentials': {
-                'error': 'ENOTALLOWED'
-            }
-        }
-
-        job = EC2TestingJob(**self.job_config)
-
-        msg = 'Credentials not found in token.'
-        with raises(MashTestingException) as e:
-            job._process_credentials('fake_msg')
-
-        assert str(e.value) == msg
-
-    @patch('mash.services.testing.ec2_job.jwt')
-    def test_process_credentials_expired(self, mock_jwt):
-        mock_jwt.decode.side_effect = jwt.ExpiredSignatureError()
-
-        job = EC2TestingJob(**self.job_config)
-
-        msg = 'Token has expired, cannot retrieve credentials.'
-        with raises(MashTestingException) as e:
-            job._process_credentials('fake_msg')
-
-        assert str(e.value) == msg
-
-    @patch('mash.services.testing.ec2_job.jwt')
-    def test_process_credentials_invalid(self, mock_jwt):
-        mock_jwt.decode.side_effect = jwt.InvalidTokenError('Broken!')
-
-        job = EC2TestingJob(**self.job_config)
-
-        msg = 'Invalid token, cannot retrieve credentials: Broken!'
-        with raises(MashTestingException) as e:
-            job._process_credentials('fake_msg')
-
-        assert str(e.value) == msg
-
-    @patch('mash.services.testing.ec2_job.test_image')
+    @patch('mash.services.testing.ipa_helper.test_image')
     @patch.object(EC2TestingJob, 'send_log')
     def test_testing_run_test(self, mock_send_log, mock_test_image):
         mock_test_image.return_value = (
@@ -107,26 +29,37 @@ class TestEC2TestingJob(object):
         )
 
         job = EC2TestingJob(**self.job_config)
-        job.image_id = 'image123'
+        job.credentials = {
+            'test-aws': {
+                'access_key_id': None,
+                'secret_access_key': None,
+                'ssh_key_name': None,
+                'ssh_private_key': None
+            }
+        }
+        job.update_test_regions({'us-east-1': 'ami-123'})
         job._run_tests()
 
         mock_test_image.assert_called_once_with(
-            'ec2',
-            access_key_id=job.access_key_id,
-            account='account',
+            'EC2',
+            access_key_id=None,
             desc=job.desc,
             distro='SLES',
-            image_id='image123',
+            image_id='ami-123',
             instance_type=job.instance_type,
             log_level=30,
-            region=job.region,
-            secret_access_key=job.secret_access_key,
-            ssh_key_name=job.ssh_key_name,
-            ssh_private_key=job.ssh_private_key,
-            ssh_user=job.ssh_user,
+            region='us-east-1',
+            secret_access_key=None,
+            ssh_key_name=None,
+            ssh_private_key=None,
+            ssh_user='ec2-user',
             tests=['test_stuff']
         )
 
+        # Failed job test
+        mock_test_image.side_effect = Exception('Tests broken!')
+        job._run_tests()
         mock_send_log.assert_has_calls(
-            [call('Log file: test.log'), call('Results file: test.results')]
+            [call('Image tests failed in region: us-east-1.'),
+             call('Tests broken!')]
         )
