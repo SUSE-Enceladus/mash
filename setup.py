705d35cf2e84fe59054aa08752de31d37fa592a5
Sean Marlow <sean.marlow@suse.com>
2018-02-27 08:26:53 -0700

    Add missing requirements on ec2publishimg.

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ config = {
         'python-dateutil',
         'APScheduler>=3.3.1',
         'amqpstorm>=2.4.0',
+        'ec2publishimg',
+        'ec2uploadimg',
         'python3-ipa'
     ],
     'packages': ['mash'],
