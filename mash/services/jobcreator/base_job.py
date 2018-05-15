b6ac4ab4fe6084f3c55859a82fd5764eaee2f0ad
Sean Marlow <sean.marlow@suse.com>
2018-05-15 09:33:39 -0600

    Add job classes and factory method.
    For creating job docs based on provider.

---
+++ mash/services/jobcreator/base_job.py
@@ -0,0 +1,240 @@
+# Copyright (c) 2018 SUSE Linux GmbH.  All rights reserved.
+#
+# This file is part of mash.
+#
+# mash is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# mash is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mash.  If not, see <http://www.gnu.org/licenses/>
+#
+
+import json
+import uuid
+
+
+class BaseJob(object):
+    """
+    Base job message class.
+
+    Handles incoming job requests.
+    """
+    def __init__(
+            self, accounts_info, provider, provider_accounts,
+            provider_groups, requesting_user, last_service, utctime, image,
+            cloud_image_name, old_cloud_image_name, project,
+            image_description, distro, tests,
+            conditions=None, instance_type=None
+    ):
+        self.id = str(uuid.uuid4())
+        self.accounts_info = accounts_info
+        self.provider = provider
+        self.provider_accounts = provider_accounts
+        self.provider_groups = provider_groups
+        self.requesting_user = requesting_user
+        self.last_service = last_service
+        self.image = image
+        self.cloud_image_name = cloud_image_name
+        self.old_cloud_image_name = old_cloud_image_name
+        self.project = project
+        self.image_description = image_description
+        self.distro = distro
+        self.tests = tests
+        self.conditions = conditions
+        self.instance_type = instance_type
+        self.utctime = utctime
+
+        self.base_message = {'id': self.id, 'utctime': self.utctime}
+
+        self.post_init()
+
+    def _get_account_info(self):
+        """
+        Parse dictionary of account data from accounts file.
+
+        Implementation in child class.
+        """
+        pass
+
+    def _get_accounts_in_group(self, group):
+        """
+        Return a list of account names given the group name.
+        """
+        return self.accounts_info['groups'][group]
+
+    def get_credentials_message(self):
+        """
+        Build credentials job message.
+        """
+        accounts = []
+        for source_region, value in self.target_account_info.items():
+            accounts.append(value['account'])
+
+        credentials_message = {
+            'credentials_job': {
+                'provider': self.provider,
+                'last_service': self.last_service,
+                'provider_accounts': accounts,
+                'requesting_user': self.requesting_user
+            }
+        }
+        credentials_message['credentials_job'].update(self.base_message)
+
+        return json.dumps(credentials_message)
+
+    def get_deprecation_message(self):
+        """
+        Build deprecation job message.
+        """
+        deprecation_message = {
+            'deprecation_job': {
+                'provider': self.provider,
+                'old_cloud_image_name': self.old_cloud_image_name,
+                'deprecation_regions': self.get_deprecation_regions()
+            }
+        }
+        deprecation_message['deprecation_job'].update(self.base_message)
+
+        return json.dumps(deprecation_message)
+
+    def get_deprecation_regions(self):
+        """
+        Return list of deprecation region info.
+
+        Implementation in child class.
+        """
+        pass
+
+    def get_obs_message(self):
+        """
+        Build OBS job message.
+        """
+        obs_message = {
+            'obs_job': {
+                'image': self.image,
+                'project': self.project,
+            }
+        }
+        obs_message['obs_job'].update(self.base_message)
+
+        if self.conditions:
+            obs_message['obs_job']['conditions'] = self.conditions
+
+        return json.dumps(obs_message)
+
+    def get_pint_message(self):
+        """
+        Build pint job message.
+        """
+        pint_message = {
+            'pint_job': {
+                'provider': self.provider,
+                'cloud_image_name': self.cloud_image_name,
+                'old_cloud_image_name': self.old_cloud_image_name
+            }
+        }
+        pint_message['pint_job'].update(self.base_message)
+
+        return json.dumps(pint_message)
+
+    def get_publisher_message(self):
+        """
+        Build publisher job message.
+
+        Implementation in child class.
+        """
+        pass
+
+    def get_replication_message(self):
+        """
+        Build replication job message and publish to replication exchange.
+        """
+        replication_message = {
+            'replication_job': {
+                'image_description': self.image_description,
+                'provider': self.provider,
+                'replication_source_regions':
+                    self.get_replication_source_regions()
+            }
+        }
+        replication_message['replication_job'].update(self.base_message)
+
+        return json.dumps(replication_message)
+
+    def get_replication_source_regions(self):
+        """
+        Return a dictionary of replication source regions.
+
+        Implementation in child class.
+        """
+        pass
+
+    def get_testing_message(self):
+        """
+        Build testing job message.
+        """
+        testing_message = {
+            'testing_job': {
+                'provider': self.provider,
+                'tests': self.tests,
+                'test_regions': self.get_testing_regions()
+            }
+        }
+
+        if self.distro:
+            testing_message['testing_job']['distro'] = self.distro
+
+        if self.instance_type:
+            testing_message['testing_job']['instance_type'] = \
+                self.instance_type
+
+        testing_message['testing_job'].update(self.base_message)
+
+        return json.dumps(testing_message)
+
+    def get_testing_regions(self):
+        """
+        Return a dictionary of target test regions.
+
+        Implementation in child class.
+        """
+        pass
+
+    def get_uploader_message(self):
+        """
+        Build uploader job message.
+        """
+        uploader_message = {
+            'uploader_job': {
+                'cloud_image_name': self.cloud_image_name,
+                'provider': self.provider,
+                'image_description': self.image_description,
+                'target_regions': self.get_uploader_regions()
+            }
+        }
+        uploader_message['uploader_job'].update(self.base_message)
+
+        return json.dumps(uploader_message)
+
+    def get_uploader_regions(self):
+        """
+        Return a dictionary of target uploader regions.
+
+        Implementation in child class.
+        """
+        pass
+
+    def post_init(self):
+        """
+        Post initialization method.
+
+        Implementation in child class.
+        """
+        pass
---
+++ mash/services/jobcreator/ec2_job.py
@@ -0,0 +1,188 @@
+# Copyright (c) 2018 SUSE Linux GmbH.  All rights reserved.
+#
+# This file is part of mash.
+#
+# mash is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# mash is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mash.  If not, see <http://www.gnu.org/licenses/>
+#
+
+import json
+import random
+
+from mash.services.jobcreator.base_job import BaseJob
+
+
+class EC2Job(BaseJob):
+    """
+    Base job message class.
+
+    Handles incoming job requests.
+    """
+    def __init__(
+            self, accounts_info, provider, provider_accounts, provider_groups,
+            requesting_user, last_service, utctime, image,
+            cloud_image_name, old_cloud_image_name, project,
+            share_with, allow_copy, image_description, distro,
+            tests, conditions=None, instance_type=None
+    ):
+        self.share_with = share_with
+        self.allow_copy = allow_copy
+        self.target_account_info = {}
+
+        super(EC2Job, self).__init__(
+            accounts_info, provider, provider_accounts, provider_groups,
+            requesting_user, last_service, utctime, image, cloud_image_name,
+            old_cloud_image_name, project, image_description, distro, tests,
+            conditions, instance_type
+        )
+
+    def _get_account_info(self):
+        """
+        Returns a dictionary of accounts and regions.
+
+        The provided target_regions dictionary may contain a list
+        of groups and accounts. An account may have a list of regions.
+
+        If regions are not provided for an account the default list
+        of all regions available is used.
+
+        Example: {
+            'us-east-1': {
+                'account': 'acnt1',
+                'target_regions': ['us-east-2', 'us-west-1', 'us-west-2']
+            }
+        }
+        """
+        group_accounts = []
+        accounts = {}
+
+        # Get dictionary of account names to target regions
+        for provider_account in self.provider_accounts:
+            accounts[provider_account['name']] = \
+                provider_account['target_regions']
+
+        helper_images = self.accounts_info.get('helper_images')
+
+        # Get all accounts from all groups
+        for group in self.provider_groups:
+            group_accounts += self._get_accounts_in_group(group)
+
+        # Add accounts from groups that don't already exist
+        for account in group_accounts:
+            if account not in accounts:
+                accounts[account] = None
+
+        for account, target_regions in accounts.items():
+            if not target_regions:
+                # Get default list of all available regions for account
+                target_regions = self._get_regions_for_account(account)
+
+            # A random region is selected as source region.
+            target = random.choice(target_regions)
+            self.target_account_info[target] = {
+                'account': account,
+                'target_regions': target_regions,
+                'helper_image': helper_images[target]
+            }
+
+    def _get_regions_for_account(self, account):
+        """
+        Return a list of regions based on account name.
+        """
+        regions_key = self.accounts_info['accounts'][account]
+        return self.accounts_info['regions'][regions_key]
+
+    def _get_target_regions_list(self):
+        """
+        Return list of region info.
+        """
+        regions = []
+
+        for source_region, value in self.target_account_info.items():
+            regions.append(value)
+
+        return regions
+
+    def get_deprecation_regions(self):
+        """
+        Return list of deprecation region info.
+
+        """
+        return self._get_target_regions_list()
+
+    def get_publisher_message(self):
+        """
+        Build publisher job message.
+        """
+        publisher_message = {
+            'publisher_job': {
+                'provider': self.provider,
+                'allow_copy': self.allow_copy,
+                'share_with': self.share_with,
+                'publish_regions': self.get_publisher_regions()
+            }
+        }
+        publisher_message['publisher_job'].update(self.base_message)
+
+        return json.dumps(publisher_message)
+
+    def get_publisher_regions(self):
+        """
+        Return a list of publisher region info.
+        """
+        return self._get_target_regions_list()
+
+    def get_replication_source_regions(self):
+        """
+        Return a dictionary of replication source regions.
+        """
+        replication_source_regions = {}
+
+        for source_region, value in self.target_account_info.items():
+            replication_source_regions[source_region] = {
+                'account': value['account'],
+                'target_regions': value['target_regions']
+            }
+
+        return replication_source_regions
+
+    def get_testing_regions(self):
+        """
+        Return a dictionary of target test regions.
+        """
+        test_regions = {}
+
+        for source_region, value in self.target_account_info.items():
+            test_regions[source_region] = value['account']
+
+        return test_regions
+
+    def get_uploader_regions(self):
+        """
+        Return a dictionary of target uploader regions.
+        """
+        target_regions = {}
+
+        for source_region, value in self.target_account_info.items():
+            target_regions[source_region] = {
+                'account': value['account'],
+                'helper_image': value['helper_image']
+            }
+
+        return target_regions
+
+    def post_init(self):
+        """
+        Post initialization method.
+        """
+        self._get_account_info()
