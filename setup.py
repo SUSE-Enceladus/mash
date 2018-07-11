f64efec3351794b21bcb19e0ccf9eb4dc27d5637
Sean Marlow <sean.marlow@suse.com>
2018-07-11 10:42:40 -0600

    Remove 3.7 from travis config.
    3.7 is not yet available.
    Add supported versions 34 and 35 to setup.py.

--- setup.py
+++ setup.py
@@ -45,6 +45,8 @@ config = {
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: System :: Operating System'
     ]
