d32ae2c7fc41d50f688a240d43a62f315aa29656
Sean Marlow <sean.marlow@suse.com>
2018-10-31 11:12:50 -0600

    Add python3.7 to supported versions in setup.py.

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ config = {
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: System :: Operating System'
     ]
 }
