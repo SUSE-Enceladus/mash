1e2c38d8f6d2670d37c5fcbf18a8fdcc40e66602
Sean Marlow <sean.marlow@suse.com>
2019-04-11 14:57:05 -0500

    Remove unused uploader conventions classes.

--- test/unit/services/uploader/conventions/amazon_test.py
+++
@@ -1,9 +0,0 @@
-from mash.services.uploader.conventions.amazon import ConventionsAmazon
-
-
-class TestConventionsBase(object):
-    def setup(self):
-        self.conventions = ConventionsAmazon()
-
-    def test_is_valid_name(self):
-        self.conventions.is_valid_name('name')
--- test/unit/services/uploader/conventions/azure_test.py
+++
@@ -1,9 +0,0 @@
-from mash.services.uploader.conventions.azure import ConventionsAzure
-
-
-class TestConventionsAzure(object):
-    def setup(self):
-        self.conventions = ConventionsAzure()
-
-    def test_is_valid_name(self):
-        self.conventions.is_valid_name('name')
