73ae744e698d084b1e32e72b4f634a63fb951a98
Sean Marlow <sean.marlow@suse.com>
2019-07-30 15:28:16 -0500

    Disable flask-restplus swagger doc.
    Generate static swagger api doc endpoint.

--- mash/services/api/__init__.py
+++ mash/services/api/__init__.py
@@ -16,7 +16,9 @@
 # along with mash.  If not, see <http://www.gnu.org/licenses/>
 #
 
-from flask import Flask
+import json
+
+from flask import Flask, render_template
 from flask_restplus import Api
 
 from mash.services.api.routes.accounts import api as accounts_api
@@ -32,10 +34,12 @@ from mash.services.api.routes.jobs.azure import api as azure_jobs_api
 app = Flask(__name__, static_url_path='/static')
 api = Api(
     app,
-    version='1.0',
+    version='3.3.0',
     title='MASH API',
-    description='MASH API',
-    validate=True
+    description='MASH provides a set of endpoints for Image Release '
+                'automation into the Public Cloud Frameworks.',
+    validate=True,
+    doc=False
 )
 
 api.add_namespace(accounts_api, path='/accounts')
@@ -47,3 +51,9 @@ api.add_namespace(jobs_api, path='/jobs')
 api.add_namespace(ec2_jobs_api, path='/jobs/ec2')
 api.add_namespace(gce_jobs_api, path='/jobs/gce')
 api.add_namespace(azure_jobs_api, path='/jobs/azure')
+
+
+@app.route('/api/docs', methods=('GET', 'POST'))
+def api_doc():
+    api_spec = json.dumps(api.__schema__)
+    return render_template('api_doc.html', api_spec=api_spec)
