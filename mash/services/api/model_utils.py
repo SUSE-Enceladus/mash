ac85c2871cf260f7de13a082b8bb9ce001a48202
Sean Marlow <sean.marlow@suse.com>
2019-08-14 15:22:39 -0500

    Add remaining db models for ec2.
    Add models for CRUD of EC2 groups, regions and accounts.

--- mash/services/api/model_utils.py
+++ mash/services/api/model_utils.py
@@ -18,15 +18,21 @@
 
 from sqlalchemy.exc import IntegrityError
 
-from mash.services.api import db
+from mash.mash_exceptions import MashDBException
+from mash.services.api import app, db
 from mash.services.api.models import (
+    EC2Account,
+    EC2Group,
+    EC2Region,
     User
 )
+from mash.utils.mash_utils import handle_request
 
 
 def add_user(username, email, password):
     """
     Add new user to database and set password hash.
+
     If the user or email exists return None.
     """
     user = User(
@@ -48,6 +54,7 @@ def add_user(username, email, password):
 def verify_login(username, password):
     """
     Compare password hashes.
+
     If hashes match the user is authenticated
     and user instance is returned.
     """
@@ -62,6 +69,7 @@ def verify_login(username, password):
 def get_user_by_username(username):
     """
     Retrieve user from database if a match exists.
+
     Otherwise None is returned.
     """
     user = User.query.filter_by(username=username).first()
@@ -81,6 +89,7 @@ def get_user_email(username):
 def delete_user(username):
     """
     Delete user by username.
+
     If user does not exist return 0.
     """
     user = get_user_by_username(username)
@@ -89,5 +98,176 @@ def delete_user(username):
         db.session.delete(user)
         db.session.commit()
         return 1
+    else:
+        return 0
+
+
+def get_ec2_group(name, user_id):
+    """
+    Retrieve EC2 group for user.
+
+    If group does not exist raise exception.
+    """
+    group = EC2Group.query.filter_by(name=name, user_id=user_id).first()
+
+    if not group:
+        raise MashDBException(
+            'Group {name} does not exist for EC2'.format(name=name)
+        )
+
+    return group
+
+
+def _get_or_create_ec2_group(name, user_id):
+    """
+    Retrieve EC2 group for user.
+
+    If group does not exist create it.
+    """
+    group = EC2Group.query.filter_by(name=name, user_id=user_id).first()
+
+    if not group:
+        group = EC2Group(
+            name=name,
+            user_id=user_id
+        )
+        db.session.add(group)
+
+    return group
+
+
+def create_ec2_region(region_name, helper_image, account):
+    """
+    Create new EC2 region for EC2 account.
+    """
+    region = EC2Region(
+        name=region_name,
+        helper_image=helper_image,
+        account=account
+    )
+    db.session.add(region)
+    return region
+
+
+def create_ec2_account(
+    username,
+    account_name,
+    partition,
+    region_name,
+    credentials,
+    group_name,
+    additional_regions
+):
+    """
+    Create a new EC2 account for user.
+
+    Create a new group and additional regions if necessary.
+    """
+    data = {
+        'cloud': 'ec2',
+        'account_name': account_name,
+        'requesting_user': username,
+        'credentials': credentials
+    }
+
+    user = get_user_by_username(username)
+
+    account = EC2Account(
+        name=account_name,
+        partition=partition,
+        region=region_name,
+        user_id=user.id
+    )
+
+    if group_name:
+        group = _get_or_create_ec2_group(group_name, user.id)
+        account.group = group
+
+    if additional_regions:
+        for additional_region in additional_regions:
+            create_ec2_region(
+                additional_region['name'],
+                additional_region['helper_image'],
+                account
+            )
+
+    try:
+        handle_request(
+            app.config['CREDENTIALS_URL'],
+            'credentials',
+            'post',
+            job_data=data
+        )
+        db.session.add(account)
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+        raise
+
+    return account
+
+
+def get_ec2_accounts(username):
+    """
+    Retrieve all EC2 accounts for user.
+    """
+    user = get_user_by_username(username)
+    return user.ec2_accounts
+
+
+def get_ec2_account(name, username):
+    """
+    Get EC2 account for given user.
+    """
+    ec2_account = EC2Account.query.filter(
+        User.username == username
+    ).filter_by(name=name).first()
+
+    return ec2_account
+
+
+def get_ec2_account_by_id(name, user_id):
+    """
+    Get EC2 account for given user.
+    """
+    try:
+        ec2_account = EC2Account.query.filter_by(
+            name=name, user_id=user_id
+        ).one()
+    except Exception:
+        raise MashDBException(
+            'EC2 account {name} does not exist for EC2'.format(name=name)
+        )
+
+    return ec2_account
+
+
+def delete_ec2_account(name, username):
+    """
+    Delete EC2 account for user.
+    """
+    data = {
+        'cloud': 'ec2',
+        'account_name': name,
+        'requesting_user': username
+    }
+
+    ec2_account = get_ec2_account(name, username)
+
+    if ec2_account:
+        try:
+            db.session.delete(ec2_account)
+            db.session.commit()
+            handle_request(
+                app.config['CREDENTIALS_URL'],
+                'credentials',
+                'delete',
+                job_data=data
+            )
+        except Exception:
+            db.session.rollback()
+            raise
+        else:
+            return 1
     else:
         return 0
