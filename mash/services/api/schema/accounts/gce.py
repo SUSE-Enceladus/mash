6547bfb5feab8253f51ba99c53f658e2dc029c7a
Sean Marlow <sean.marlow@suse.com>
2019-09-19 16:07:06 -0500

    Add gce account update endpoint.
    For updating gce account info and credentials.

--- mash/services/api/schema/accounts/gce.py
+++ mash/services/api/schema/accounts/gce.py
@@ -86,3 +86,20 @@ gce_credentials = {
 add_account_gce = copy.deepcopy(gce_account)
 add_account_gce['properties']['credentials'] = gce_credentials
 add_account_gce['required'].append('credentials')
+
+gce_account_update = {
+    'type': 'object',
+    'properties': {
+        'bucket': string_with_example('image-bucket'),
+        'testing_account': string_with_example('testing-account1'),
+        'region': string_with_example('us-west1-a'),
+        'credentials': gce_credentials
+    },
+    'additionalProperties': False,
+    'anyOf': [
+        {'required': ['bucket']},
+        {'required': ['testing_account']},
+        {'required': ['region']},
+        {'required': ['credentials']}
+    ]
+}
