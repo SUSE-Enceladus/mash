7d33f10e4af429274636a8868c2d4a5b01d5226c
Sean Marlow <sean.marlow@suse.com>
2020-02-04 14:35:29 -0600

    Add oci job args for image creation.
    Update oci job json example.

--- mash/services/api/schema/jobs/oci.py
+++ mash/services/api/schema/jobs/oci.py
@@ -29,6 +29,16 @@ oci_job_message['properties']['bucket'] = string_with_example('images')
 oci_job_message['properties']['region'] = string_with_example('us-phoenix-1')
 oci_job_message['properties']['availability_domain'] = string_with_example('Omic:PHX-AD-1')
 oci_job_message['properties']['compartment_id'] = string_with_example('ocid1.compartment.oc1..')
+oci_job_message['properties']['operating_system'] = string_with_example('SLES')
+oci_job_message['properties']['operating_system_version'] = string_with_example('12SP1')
+oci_job_message['properties']['image_type'] = {
+    'type': 'string',
+    'enum': ['QCOW2', 'VMDK']
+}
+oci_job_message['properties']['launch_mode'] = {
+    'type': 'string',
+    'enum': ['NATIVE', 'EMULATED', 'PARAVIRTUALIZED', 'CUSTOM']
+}
 oci_job_message['properties']['image']['example'] = 'openSUSE-Leap-15.0-oci'
 oci_job_message['properties']['cloud_image_name']['example'] = \
     'opensuse-leap-15-v{date}'
@@ -39,3 +49,5 @@ oci_job_message['properties']['image_description']['example'] = \
 oci_job_message['properties']['instance_type']['example'] = 'VM.Standard2.1'
 
 oci_job_message['required'].append('cloud_account')
+oci_job_message['required'].append('operating_system')
+oci_job_message['required'].append('operating_system_version')
