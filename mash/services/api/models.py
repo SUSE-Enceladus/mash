0aea54a5ffde54ded60ae3d179e35189a3df55dd
Joachim Gleissner <jgleissner@suse.com>
2020-03-05 16:05:41 +0000

    use email in User.__repr__

--- mash/services/api/models.py
+++ mash/services/api/models.py
@@ -76,7 +76,7 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.id)
+        return '<User {}>'.format(self.email)
 
 
 class Token(db.Model):
