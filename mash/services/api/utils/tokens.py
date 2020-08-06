fe34735672b5c629a8b704287fcacd748e216b76
Sean Marlow <sean.marlow@suse.com>
2020-08-06 12:27:30 -0500

    Migrate remaining token routes to db service.
    All database actions move to database service.

--- mash/services/api/utils/tokens.py
+++ mash/services/api/utils/tokens.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 SUSE LLC.  All rights reserved.
+# Copyright (c) 2020 SUSE LLC.  All rights reserved.
 #
 # This file is part of mash.
 #
@@ -16,24 +16,10 @@
 # along with mash.  If not, see <http://www.gnu.org/licenses/>
 #
 
-from datetime import datetime
+from flask import current_app
 from flask_jwt_extended import decode_token
 
-from sqlalchemy.orm.exc import NoResultFound
-
-from mash.services.database.extensions import db
-from mash.services.database.models import (
-    User,
-    Token
-)
-from mash.services.api.utils.users import get_user_by_id
-
-
-def _epoch_utc_to_datetime(epoch_utc):
-    """
-    Convert epoch timestamp into datetime.
-    """
-    return datetime.fromtimestamp(epoch_utc)
+from mash.utils.mash_utils import handle_request
 
 
 def add_token_to_database(encoded_token, user_id):
@@ -43,20 +29,19 @@ def add_token_to_database(encoded_token, user_id):
     decoded_token = decode_token(encoded_token)
     jti = decoded_token['jti']
     token_type = decoded_token['type']
+    expires = decoded_token['exp']
 
-    if 'exp' in decoded_token:
-        expires = _epoch_utc_to_datetime(decoded_token['exp'])
-    else:
-        expires = None
-
-    token = Token(
-        jti=jti,
-        token_type=token_type,
-        user_id=user_id,
-        expires=expires
+    handle_request(
+        current_app.config['DATABASE_API_URL'],
+        'tokens/',
+        'post',
+        job_data={
+            'jti': jti,
+            'token_type': token_type,
+            'user_id': user_id,
+            'expires': expires
+        }
     )
-    db.session.add(token)
-    db.session.commit()
 
 
 def is_token_revoked(decoded_token):
@@ -64,10 +49,12 @@ def is_token_revoked(decoded_token):
     Checks if the given token exists.
     """
     jti = decoded_token['jti']
+    user_id = decoded_token['identity']
+    token = get_token_by_jti(jti, user_id)
 
-    try:
-        Token.query.filter_by(jti=jti).one()
-    except NoResultFound:
+    if token:
+        return False
+    else:
         return True
 
 
@@ -75,53 +62,55 @@ def get_user_tokens(user_id):
     """
     Returns all of the tokens for given user.
     """
-    user = get_user_by_id(user_id)
-    return user.tokens
+    response = handle_request(
+        current_app.config['DATABASE_API_URL'],
+        'tokens/list/{user}'.format(user=user_id),
+        'get'
+    )
+
+    return response.json()
 
 
 def get_token_by_jti(token_jti, user_id):
     """
     Get token by jti identifier.
     """
-    user = get_user_by_id(user_id)
-    token = Token.query.filter(
-        User.id == user.id
-    ).filter_by(jti=token_jti).first()
+    response = handle_request(
+        current_app.config['DATABASE_API_URL'],
+        'tokens/',
+        'get',
+        job_data={
+            'jti': token_jti,
+            'user_id': user_id
+        }
+    )
 
-    return token
+    return response.json()
 
 
 def revoke_token_by_jti(jti, user_id):
     """
     Revoke token by jti identifier.
     """
-    token = get_token_by_jti(jti, user_id)
-
-    if token:
-        db.session.delete(token)
-        db.session.commit()
-        return 1
-    else:
-        return 0
+    response = handle_request(
+        current_app.config['DATABASE_API_URL'],
+        'tokens/',
+        'delete',
+        job_data={
+            'jti': jti,
+            'user_id': user_id
+        }
+    )
+    return response.json()['rows_deleted']
 
 
 def revoke_tokens(user_id):
     """
     Revokes (deletes) all tokens for given user.
     """
-    user = get_user_by_id(user_id)
-    rows_deleted = len(user.tokens)
-    user.tokens = []
-    db.session.commit()
-    return rows_deleted
-
-
-def prune_expired_tokens():
-    """
-    Delete tokens that have expired from the database.
-    """
-    now = datetime.now()
-    rows_deleted = Token.query.filter(Token.expires < now).delete()
-    db.session.commit()
-
-    return rows_deleted
+    response = handle_request(
+        current_app.config['DATABASE_API_URL'],
+        'tokens/list/{user}'.format(user=user_id),
+        'delete'
+    )
+    return response.json().get('rows_deleted', 0)
