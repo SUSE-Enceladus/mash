d26b5f49ac54e1b9be5aa7235bc6ef32b1acd79e
Sean Marlow <sean.marlow@suse.com>
2020-08-12 14:22:39 -0500

    Migrate base job routes to database service.

--- test/unit/services/api/routes/jobs/base_job_routes_test.py
+++ test/unit/services/api/routes/jobs/base_job_routes_test.py
@@ -1,4 +1,5 @@
-from unittest.mock import Mock, patch
+from datetime import datetime
+from unittest.mock import patch, Mock
 
 
 @patch('mash.services.api.routes.jobs.delete_job')
@@ -36,81 +37,97 @@ def test_api_delete_job(
         '/jobs/12345678-1234-1234-1234-123456789012'
     )
     assert response.status_code == 400
-    assert response.data == b'{"msg":"Delete job failed"}\n'
+    assert response.data == b'{"msg":"Broken"}\n'
 
 
-@patch('mash.services.api.routes.jobs.get_job')
+@patch('mash.services.api.utils.jobs.handle_request')
 @patch('mash.services.api.routes.jobs.get_jwt_identity')
 @patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
 def test_api_get_job(
         mock_jwt_required,
         mock_jwt_identity,
-        mock_get_job,
+        mock_handle_request,
         test_client
 ):
-    job = Mock()
-    job.job_id = '12345678-1234-1234-1234-123456789012'
-    job.last_service = 'test'
-    job.utctime = 'now'
-    job.image = 'test_image_oem'
-    job.download_url = 'http://download.opensuse.org/repositories/Cloud:Tools/images'
-    job.cloud_architecture = 'x86_64'
-    job.profile = 'Server'
+    job = {
+        'job_id': '12345678-1234-1234-1234-123456789012',
+        'last_service': 'test',
+        'utctime': 'now',
+        'image': 'test_image_oem',
+        'download_url': 'http://download.opensuse.org/repositories/Cloud:Tools/images',
+        'cloud_architecture': 'x86_64',
+        'profile': 'Server',
+        'start_time': str(datetime(2011, 11, 11, 11, 11, 11)),
+        'state': 'pending',
+        'errors': []
+    }
+    response = Mock()
+    response.json.return_value = job
+    mock_handle_request.return_value = response
 
-    mock_get_job.return_value = job
     mock_jwt_identity.return_value = 'user1'
 
-    response = test_client.get(
+    result = test_client.get(
         '/jobs/12345678-1234-1234-1234-123456789012'
     )
 
-    assert response.status_code == 200
-    assert response.json['job_id'] == '12345678-1234-1234-1234-123456789012'
-    assert response.json['last_service'] == 'test'
-    assert response.json['utctime'] == 'now'
-    assert response.json['image'] == 'test_image_oem'
-    assert response.json['download_url'] == 'http://download.opensuse.org/repositories/Cloud:Tools/images'
-    assert response.json['cloud_architecture'] == 'x86_64'
-    assert response.json['profile'] == 'Server'
+    assert result.status_code == 200
+    assert result.json['job_id'] == '12345678-1234-1234-1234-123456789012'
+    assert result.json['last_service'] == 'test'
+    assert result.json['utctime'] == 'now'
+    assert result.json['image'] == 'test_image_oem'
+    assert result.json['download_url'] == 'http://download.opensuse.org/repositories/Cloud:Tools/images'
+    assert result.json['cloud_architecture'] == 'x86_64'
+    assert result.json['profile'] == 'Server'
+    assert result.json['state'] == 'pending'
+    assert result.json['start_time'] == '2011-11-11 11:11:11'
 
     # Not found
-    mock_get_job.return_value = None
+    response.json.return_value = {}
 
-    response = test_client.get(
+    result = test_client.get(
         '/jobs/12345678-1234-1234-1234-123456789012'
     )
-    assert response.status_code == 404
-    assert response.data == b'{"msg":"Job not found"}\n'
+    assert result.status_code == 404
+    assert result.data == b'{"msg":"Job not found"}\n'
 
 
-@patch('mash.services.api.routes.jobs.get_jobs')
+@patch('mash.services.api.utils.jobs.handle_request')
 @patch('mash.services.api.routes.jobs.get_jwt_identity')
 @patch('flask_jwt_extended.view_decorators.verify_jwt_in_request')
 def test_api_get_job_list(
         mock_jwt_required,
         mock_jwt_identity,
-        mock_get_jobs,
+        mock_handle_request,
         test_client
 ):
-    job = Mock()
-    job.job_id = '12345678-1234-1234-1234-123456789012'
-    job.last_service = 'test'
-    job.utctime = 'now'
-    job.image = 'test_image_oem'
-    job.download_url = 'http://download.opensuse.org/repositories/Cloud:Tools/images'
-    job.cloud_architecture = 'x86_64'
-    job.profile = 'Server'
+    job = {
+        'job_id': '12345678-1234-1234-1234-123456789012',
+        'last_service': 'test',
+        'utctime': 'now',
+        'image': 'test_image_oem',
+        'download_url': 'http://download.opensuse.org/repositories/Cloud:Tools/images',
+        'cloud_architecture': 'x86_64',
+        'profile': 'Server',
+        'start_time': str(datetime(2011, 11, 11, 11, 11, 11)),
+        'state': 'pending',
+        'errors': []
+    }
+    response = Mock()
+    response.json.return_value = [job]
+    mock_handle_request.return_value = response
 
-    mock_get_jobs.return_value = [job]
     mock_jwt_identity.return_value = 'user1'
 
-    response = test_client.get('/jobs/')
+    result = test_client.get('/jobs/')
 
-    assert response.status_code == 200
-    assert response.json[0]['job_id'] == '12345678-1234-1234-1234-123456789012'
-    assert response.json[0]['last_service'] == 'test'
-    assert response.json[0]['utctime'] == 'now'
-    assert response.json[0]['image'] == 'test_image_oem'
-    assert response.json[0]['download_url'] == 'http://download.opensuse.org/repositories/Cloud:Tools/images'
-    assert response.json[0]['cloud_architecture'] == 'x86_64'
-    assert response.json[0]['profile'] == 'Server'
+    assert result.status_code == 200
+    assert result.json[0]['job_id'] == '12345678-1234-1234-1234-123456789012'
+    assert result.json[0]['last_service'] == 'test'
+    assert result.json[0]['utctime'] == 'now'
+    assert result.json[0]['image'] == 'test_image_oem'
+    assert result.json[0]['download_url'] == 'http://download.opensuse.org/repositories/Cloud:Tools/images'
+    assert result.json[0]['cloud_architecture'] == 'x86_64'
+    assert result.json[0]['profile'] == 'Server'
+    assert result.json[0]['state'] == 'pending'
+    assert result.json[0]['start_time'] == '2011-11-11 11:11:11'
