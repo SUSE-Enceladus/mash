a34b1c326ffefd3fe33b922e39d471cec12557ba
Joachim Gleissner <jgleissner@suse.com>
2020-08-14 19:16:05 +0100

    Add MASH cleanup service
    Handles purging of old image files

---
+++ mash/services/cleanup/config.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2020 SUSE Linux GmbH.  All rights reserved.
+#
+# This file is part of mash.
+#
+# mash is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# mash is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mash.  If not, see <http://www.gnu.org/licenses/>
+#
+
+from mash.services.base_config import BaseConfig
+from mash.services.cleanup.defaults import Defaults as CleanupDefaults
+from mash.services.obs.defaults import Defaults as OBSDefaults
+
+
+class CleanupConfig(BaseConfig):
+    """
+    Implements reading of cleanup configuration from mash configuration file:
+
+    * /etc/mash/mash_config.yaml
+
+    The mash configuration file is a yaml formatted file containing
+    information to control the behavior of the mash services.
+    """
+    def __init__(self, config_file=None):
+        super(CleanupConfig, self).__init__(config_file)
+
+    def get_download_directory(self):
+        """
+        Return directory name for image download directory:
+
+        obs:
+          download_directory: /tmp
+
+        if no configuration exists the download dir name from
+        the Defaults class is returned
+
+        :rtype: string
+        """
+        download_directory = self._get_attribute(
+            attribute='download_directory', element='obs'
+        )
+        return download_directory if download_directory else \
+            OBSDefaults.get_download_dir()
+
+    def get_max_image_age(self):
+        """
+        Return maximum image age (in days):
+
+        cleanup:
+          max_image_age: 90
+
+        if no configuration exists the max image age from
+        the Defaults class is returned
+
+        :rtype: int
+        """
+        max_image_age = self._get_attribute(
+            attribute='max_image_age', element='cleanup'
+        )
+        return max_image_age if max_image_age else \
+            CleanupDefaults.get_max_image_age()
