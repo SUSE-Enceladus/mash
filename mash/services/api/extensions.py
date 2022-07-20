3a7f96b1bf33873182142569bab33d74af9668cd
Sean Marlow <sean.marlow@suse.com>
2022-07-20 08:14:22 -0400

    Bump version: 13.2.0 → 13.2.1

--- mash/services/api/extensions.py
+++ mash/services/api/extensions.py
@@ -31,7 +31,7 @@ authorizations = {
 }
 
 api = Api(
-    version='13.2.0',
+    version='13.2.1',
     contact='public-cloud-dev@susecloud.net',
     title='MASH API',
     description='MASH provides a set of endpoints for Image Release '
